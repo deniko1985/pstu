87525d9e575d001defad78423fdec5c1e392135a
deniko1985 <deniko1985@gmail.com>
2022-06-24 10:19:28 +0500

    Update lesson_4_2.py
    renamed classes

--- lesson_4/lesson_4_2.py
+++ lesson_4/lesson_4_2.py
@@ -7,7 +7,7 @@ import unittest
 class CompareGeneral():
     pass
 
-class CompareByMethod1(CompareGeneral):
+class СountCompare(CompareGeneral):
 
     def compare(self, S1, S2):
         ngrams = [S1[i:i+3] for i in range(len(S1))]
@@ -16,32 +16,31 @@ class CompareByMethod1(CompareGeneral):
             count += S2.count(ngram)
         return S1, S2, count / max(len(S1), len(S2))
 
-class CompareByMethod2(CompareGeneral):
+class JaroWinklerCompare(CompareGeneral):
 
     def compare(self, S1, S2):
         return S1, S2, JaroWinkler().similarity(S1, S2)
 
-class CompareByMethod3(CompareGeneral):
+class FuzzywuzzyCompare(CompareGeneral):
 
     def compare(self, S1, S2):
         return S1, S2, fuzz.WRatio(S1, S2)/100
 
-class CompareByMethod4(CompareGeneral):
+class LevenshteinCompare(CompareGeneral):
 
     def compare(self, S1, S2):
         return S1, S2, NormalizedLevenshtein().distance(S1, S2)
 
 if __name__ == '__main__':
     class TestCompare (unittest.TestCase):
-        def test_CompareByMethod(self):
-            self.assertEqual(CompareByMethod1.compare(self, 'море', 'гора'), ('море', 'гора', 0.0))  
-            self.assertEqual(CompareByMethod1.compare(self, 'компьютер', 'компьютеризация'), ('компьютер', 'компьютеризация', 0.6))
-            self.assertEqual(CompareByMethod2.compare(self, 'море', 'гора'), ('море', 'гора', 0.6666666666666666))
-            self.assertEqual(CompareByMethod2.compare(self, 'компьютер', 'компьютеризация'), ('компьютер', 'компьютеризация', 0.9466666666666667))
-            self.assertEqual(CompareByMethod3.compare(self, 'море', 'гора'), ('море', 'гора', 0.5))  
-            self.assertEqual(CompareByMethod3.compare(self, 'компьютер', 'компьютеризация'), ('компьютер', 'компьютеризация', 0.9))
-            self.assertEqual(CompareByMethod4.compare(self, 'море', 'гора'), ('море', 'гора', 0.5))  
-            self.assertEqual(CompareByMethod4.compare(self, 'компьютер', 'компьютеризация'), ('компьютер', 'компьютеризация', 0.4))                     
+        def test_CompareByMethod1(self):
+            self.assertEqual(СountCompare.compare(self, 'море', 'гора'), ('море', 'гора', 0.0))  
+            self.assertEqual(СountCompare.compare(self, 'компьютер', 'компьютеризация'), ('компьютер', 'компьютеризация', 0.6))
+            self.assertEqual(JaroWinklerCompare.compare(self, 'море', 'гора'), ('море', 'гора', 0.6666666666666666))
+            self.assertEqual(JaroWinklerCompare.compare(self, 'компьютер', 'компьютеризация'), ('компьютер', 'компьютеризация', 0.9466666666666667))
+            self.assertEqual(FuzzywuzzyCompare.compare(self, 'море', 'гора'), ('море', 'гора', 0.5))  
+            self.assertEqual(FuzzywuzzyCompare.compare(self, 'компьютер', 'компьютеризация'), ('компьютер', 'компьютеризация', 0.9))
+            self.assertEqual(LevenshteinCompare.compare(self, 'море', 'гора'), ('море', 'гора', 0.5))  
+            self.assertEqual(LevenshteinCompare.compare(self, 'компьютер', 'компьютеризация'), ('компьютер', 'компьютеризация', 0.4))                     
     
     unittest.main()
-
